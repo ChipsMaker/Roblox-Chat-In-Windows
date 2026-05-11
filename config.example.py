5c982fe18f92af63fc208150f1ea9b5e5e79d03a
Chips <voprosnik.vopros@mail.ru>
2026-05-11 16:26:36 +0800

    0.7.3

--- config.example.py
+++ config.example.py
@@ -3,7 +3,7 @@ import base64
 import hashlib
 SERVERS_DATA = {...}
 SERVER_URLS = list(SERVERS_DATA.keys())
-VERSION = '0.7.2'
+VERSION = '0.7.3'
 GITHUB_REPO = 'ChipsMaker/Roblox-Chat-In-Windows'
 
 def generate_room_key() -> str:
